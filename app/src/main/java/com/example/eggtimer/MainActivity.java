8d9ee7c4c98a652792599d798f6661a3840b51d3
Yash Lad <yashlad727@gmail.com>
2022-10-07 18:29:37 +0530

    .start() error with buttonclicked fun

--- app/src/main/java/com/example/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -3,11 +3,47 @@ package com.example.eggtimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    SeekBar seekBar;
+    public void buttonClicked(View view){
+        Log.i("Button Pressed!", "Nice!");
+
+        CountDownTimer countDownTimer = new CountDownTimer(seekBar.getProgress()*1000, 1000) {
+            @Override
+            public void onTick(long l) {
+                updateTimeFun((int)l/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                Log.i("Finished", "Timer all Done");
+            }
+        };
+
+    }.start();
+
+    public void updateTimeFun(int secondsleft){
+        int minutes = secondsleft /60;
+        int seconds = secondsleft - (minutes * 60);
+
+        String secondString = Integer.toString(seconds);
+
+        if(secondString.equals("0")){
+            secondString = "00";
+        }
+
+        //textView.setText(Integer.toString(minutes) + ":" + Integer.toString(seconds));
+
+        textView.setText(Integer.toString(minutes) + ":" + secondString);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView imageView = findViewById(R.id.EggimageView);
-        SeekBar seekBar = findViewById(R.id.timerseekBar);
-        TextView textView = findViewById(R.id.CountDowntextView);
+        seekBar = findViewById(R.id.timerseekBar);
+        textView = findViewById(R.id.CountDowntextView);
 
         int max = 600;
         int startingPos = 30;
@@ -27,18 +63,7 @@ public class MainActivity extends AppCompatActivity {
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                int minutes = i /60;
-                int seconds = i - (minutes * 60);
-
-                String secondString = Integer.toString(seconds);
-
-                if(secondString == "0"){
-                    secondString = "00";
-                }
-
-                //textView.setText(Integer.toString(minutes) + ":" + Integer.toString(seconds));
-
-                textView.setText(Integer.toString(minutes) + ":" + secondString);
+                updateTimeFun(i);
             }
 
             @Override
